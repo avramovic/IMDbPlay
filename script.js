6c28556c47d3f9358a7825b241f4a17ad1bbf661
Nemanja <nemanja.avramovic@partech.com>
2024-12-02 18:55:40 +0100

    cleanup

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-console.log("IMDb Play Icon Script Running!");
-
 const observer = new MutationObserver(() => {
   const pageTitle = document.querySelector("h1");
   console.log(pageTitle);
   if (pageTitle && !pageTitle.querySelector('.play-icon')) {
-    console.log("Target element found:", pageTitle);
-
     const playIcon = document.createElement("span");
     playIcon.className = "play-icon"; // Add a class to avoid duplicates
     playIcon.textContent = " ▶️";
     playIcon.style.cursor = "pointer";
-    playIcon.style.color = "green";
     playIcon.title = "Click to play in an overlay, ctrl+click (cmd+click on Mac) to open in a new tab.";
     playIcon.addEventListener("click", (e) => {
       const pathSegments = window.location.pathname.split('/').filter(segment => segment);
       let imdb_id = pathSegments[1];
       let media_type = document.querySelector('meta[property="og:type"]')?.content;
-      let base_url = localStorage.getItem('lightboxBaseUrl') || 'https://vidsrc.xyz/embed/';
-
-      if (!base_url.endsWith('/')) {
-        base_url += '/';
-      }
+      let base_url = 'https://vidsrc.xyz/embed/';
 
       let video_url = base_url + '/movie/' + imdb_id;
       if (media_type === 'video.tv_show') {
@@ -38,9 +29,7 @@ const observer = new MutationObserver(() => {
   }
 });
 
-// Start observing the body for changes
 observer.observe(document.body, {childList: true, subtree: true});
-
 
 function createLightbox(iframeSrc) {
   // Create lightbox container
